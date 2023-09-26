4aa71f03185600c8d0ae945d350d17a6b0c739e7
bloodmask <kiremate@hotmail.com>
2023-09-26 12:48:46 +0200

    Created a Factory Pattern for races so we can load and create Races loading its data from a json file so we don't hardcode Race information.
    We'll probably need to do the same to Stats in a future to avoid having hardcoded strings inside the class.

---
+++ Races/Derived/Human.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodmaskCharacterCalculator.Races.Derived
+{
+    public  class Human : Race
+    {
+        #region Constructors
+        public Human() : base("Human", "Enhanced Intelligence, Environmental Adaptability", "Humans in the Bloodmask universe possess a natural affinity for complex problem-solving, which grants them a bonus to Intelligence stats. Their uncanny ability to adapt technologies to different biomes gives them a bonus to Endurance when facing environmental challenges.") { }
+        #endregion
+    }
+}
---
+++ Races/Race.cs
@@ -0,0 +1,109 @@
+﻿using BloodmaskCharacterCalculator.Races.Derived;
+using BloodmaskCharacterCalculator.Stats;
+using BloodmaskCharacterCalculator.Stats.Common;
+using BloodmaskCharacterCalculator.Stats.Power;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodmaskCharacterCalculator.Races
+{
+    /// <summary>
+    /// Will contain all the information related to race as well as the default stats
+    /// </summary>
+    public enum E_Races { Human, Demon, Lurker, Undead, Primordial, Colossus, Nordic };
+    public class Race
+    {
+        protected string _name;
+        protected string _perks;
+        protected string _backgroundStory;
+        protected E_Races _type;
+        protected List<Stat> _defaultStats;
+        #region Getters & Setters
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Perks 
+        {
+            get { return _perks;}
+            set { _perks = value; }
+        }
+        public string BackgroundStory
+        {
+            get { return _backgroundStory; }
+            set { _backgroundStory = value; }
+        }
+        public E_Races Type { get { return _type; } }
+        public List<Stat> DefaultStats
+        {
+            get { return _defaultStats; }
+            set { _defaultStats = value; }
+        }
+        #endregion
+        #region Constructors
+        // Empty Constructor
+        public Race()
+        {
+            _defaultStats = new List<Stat>();
+            InitializateDefaultStats();
+        }
+        // Parameterized Constructor
+        public Race(string name, E_Races type, string perks, string backgroundStory, List<Stat> additionalStats = null)
+        {
+            _name = name;
+            _type = type;
+            _perks = perks;
+            _backgroundStory = backgroundStory;
+            _defaultStats = additionalStats ?? new List<Stat>(); // We add the additionalStats if not null to the current default stats
+            InitializateDefaultStats();
+        }
+        #endregion 
+        // This function will be called each time a Race is initializated
+        protected virtual void InitializateDefaultStats()
+        {
+            // Since all the Races will have Common and Power stats we initialize those
+            #region Common Stats
+            // Values may change through the dev process
+            _defaultStats.Add(new Health(42)); // We give 42 default hp in honor to the Mottled Boar in WoW
+            _defaultStats.Add(new Vigor(100)); // We give 100 default energy in honor to the Rogue class in WoW
+            #endregion
+            #region Power Stats
+            // We give 15 as is the minimum power stat in WoW 
+            _defaultStats.Add(new Strength(15)); 
+            _defaultStats.Add(new Dexterity(15));
+            _defaultStats.Add(new Intelligence(15));
+            #endregion
+        }
+        // Factory Pattern
+        public static Race CreateRaceFromJson(string json)
+        {
+            var race = JsonConvert.DeserializeObject<Race>(json);
+            // This will create a certain Race depending on it's type
+            switch (race.Type)
+            {
+                case E_Races.Human:
+                    return JsonConvert.DeserializeObject<Human>(json);
+                case E_Races.Demon:
+                    return JsonConvert.DeserializeObject<Human>(json);
+                case E_Races.Lurker:
+                    return JsonConvert.DeserializeObject<Human>(json);
+                case E_Races.Undead:
+                    return JsonConvert.DeserializeObject<Human>(json);
+                case E_Races.Primordial:
+                    return JsonConvert.DeserializeObject<Human>(json);
+                case E_Races.Colossus:
+                    return JsonConvert.DeserializeObject<Human>(json);
+                case E_Races.Nordic:
+                    return JsonConvert.DeserializeObject<Human>(json);
+
+                default: // This would be a weird case
+                    return race;
+            }
+        }
+    }
+}
